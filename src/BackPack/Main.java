32653fad669c0d57c87df7a6059ff18521fadd36
vev07 <108584083+vev07@users.noreply.github.com>
2023-04-15 19:31:06 +0200

    .

---
+++ src/BackPack/Main.java
@@ -0,0 +1,27 @@
+package BackPack;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] weights = {3,4,5,8,9};
+        int[] prices = {1,6,4,7,6};
+        int maxSizeBP = 13;
+
+
+    }
+    public static int searchMaxSize(int[] weights, int[] prices, int maxSizeBP) {
+        int[] thinkValue = new int[weights.length];
+        int theMostValue = 0;
+        for (int i = 0; i < weights.length; i++) {
+            thinkValue[i] = weights[i] * prices[i];
+            } if ()
+
+
+        }
+
+
+//        int value = 0;
+//        while (value <= maxSizeBP) {
+//
+//        }
+    }
+}
